c72ecde101b3b80eb614485e0676abcda38990e9
Alexandr Logvinov <for@kayama.me>
2023-07-24 14:46:19 +0200

    feat: improve notifications (#2)
    coloring Status notifications

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,10 @@
 export const CONCLUSION_THEMES: any = {
   neutral: "5DB1D1",
-  success: "90C978",
+  success: "00FF66",
   skipped: "DEDEDE",
   cancelled: "D99294",
   timed_out: "FF756D",
-  failure: "C23B23",
+  failure: "FF3333",
   action_required: "FF3636",
+  in_progress: "5DB1D1",
 };
